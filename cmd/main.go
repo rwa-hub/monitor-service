2a22154ace10a04841ec6a8f1d4e7734ed7adbcb
renancorreadev <skynancemusic@gmail.com>
2025-03-13 01:49:10 +0000

    feat: finish event streaming from hyperledger besu

--- cmd/main.go
+++ cmd/main.go
@@ -2,18 +2,27 @@ package main
 
 import (
 	"log"
+	"net/http"
+
 	"monitor-service/internal/adapters/rpc"
 	"monitor-service/internal/adapters/websocket"
-	"monitor-service/internal/app/service"
+	service "monitor-service/internal/service"
 )
 
 func main() {
 	rpcURL := "ws://host.docker.internal:8546"
-	contractAddress := "0xFa5B6432308d45B54A1CE1373513Fab77166436f"
+	contractAddresses := map[string]string{
+		"financial_compliance":   "0x486B9f3Fb089f9166147149562e0A6952442Fa42",
+		"modular_compliance":     "0x9F035Be9853eb3Fc10155361fda197C159eFDD09",
+		"ident_registry_storage": "0xf935973e9f884c66e6E3ce681344064e565e0250",
+		"registry_md":            "0xC8c03647d39a96f02f6Ce8999bc22493C290e734",
+	}
 
+	// Inicializa o servidor WebSocket
 	wsServer := websocket.NewWebSocketServer()
-	go wsServer.HandleMessages()
+	http.HandleFunc("/ws", wsServer.HandleConnections)
 
+	// Inicializa a conexão RPC
 	rpcClient, err := rpc.NewRPCClient(rpcURL)
 	if err != nil {
 		log.Fatalf("❌ Erro ao conectar ao nó Ethereum: %v", err)
@@ -25,7 +34,16 @@ func main() {
 		}
 	}()
 
-	service.EventService(rpcClient, wsServer, contractAddress)
+	// Inicia os serviços de eventos
+	go service.EventService(rpcClient, wsServer, contractAddresses)
 
-	select {}
+	// Define a porta do WebSocket
+	port := ":8080"
+	log.Printf("✅ Servidor WebSocket iniciado na porta %s", port)
+
+	// Inicia o servidor HTTP para WebSocket
+	err = http.ListenAndServe(port, nil)
+	if err != nil {
+		log.Fatalf("❌ Erro ao iniciar o servidor HTTP: %v", err)
+	}
 }
